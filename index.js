de851cf66ed22f0d56e2cfb7d501610b96055610
bengroves <bengroves15@gmail.com>
2021-03-17 14:07:17 -0400

    commit tree 41e98841681e8a0f02d9c5360b53f54774a29ed7

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+
+let num1 = 1
+let num2 = 62
+let multiply = num1*num2
+let num3 = 15
+let num4 = 4
+let mod = (num3 % num4) + 1
+let max = Math.max(1,2,20)
+
+let random = 1
